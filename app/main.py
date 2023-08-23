5999ca2d8c479d9d4a17c508516d98738a2881de
mohammadreza <mohammad78t@gmail.com>
2023-08-23 18:48:52 +0330

    add Dockerfile and fix error in url HTTPS Template

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+import uvicorn
 from fastapi.templating import Jinja2Templates
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
@@ -6,6 +9,16 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+def https_url_for(request: Request, name: str, **path_params: Any) -> str:
+    http_url = request.url_for(name, **path_params)
+    # Replace 'http' with 'https'
+    scheme = request._headers.get('x-forwarded-proto', 'http')
+    return http_url.replace(scheme=scheme)
+
+
+templates.env.globals["https_url_for"] = https_url_for
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -17,3 +30,7 @@ async def root(request: Request):
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
